8d41b667864939ea0657b412bbcc05d65345e9db
SHAIK KHALEEL BASHA <khaleel83745@gmail.com>
2025-09-04 12:48:31 +0530

    commit push

---
+++ KodnestFinalProject/src/main/java/com/Kodnest/KodnestFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Kodnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodnestFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodnestFinalProjectApplication.class, args);
+	}
+
+}
---
+++ KodnestFinalProject/src/test/java/com/Kodnest/KodnestFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kodnest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodnestFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
